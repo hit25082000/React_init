f8ef0ab438dc594df8886dacad17fa51da567784
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2021-12-21 14:45:43 -0400

    React_init: webpack dev server ✔, convertendo e atualizando automaticamente com yarn webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
+	mode: 'development',
 	entry: path.resolve(__dirname, 'src', 'index.jsx'),
 	output:{
 		path: path.resolve(__dirname,'dist'),
@@ -9,6 +12,18 @@ module.exports = {
 	resolve: {
 		extensions: ['.js','.jsx'],
 	},
+	devServer: {
+		static: {
+		  directory: path.join(__dirname, 'public'),
+		},
+		compress: true,
+		port: 9000,
+	  },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html')
+		})
+	],	
 	module: {
 		rules: [
 			{
