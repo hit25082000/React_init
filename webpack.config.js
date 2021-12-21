90ed32e21427a094b477ec569efd1bb282eadb01
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2021-12-21 13:57:09 -0400

    React_init: webpack bundle configurados pronto para ir ao browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [',js','.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            }
-        ]
-    }
+	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	output:{
+		path: path.resolve(__dirname,'dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js','.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			}
+		]
+	}
 };
